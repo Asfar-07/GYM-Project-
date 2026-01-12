f563848d1fddd2c71929b07b0ef6e343fd5ab51d
Asfar-07 <asfarbismi@gmail.com>
2026-01-12 23:52:13 +0530

    add google auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import {Provider} from "react-redux"
 import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root1'));
 root.render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId="">
     <Provider store={store}>
     <App />
     </Provider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,14 @@
-import './login.css'
+import "./login.css";
+import { GoogleLogin } from "@react-oauth/google";
 import React, { useState } from "react";
 import { FaGoogle, FaFacebookF } from "react-icons/fa";
 
 export default function Login() {
-     const [isSignup, setIsSignup] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
+  const handleSuccess = async (credentialResponse) => {
+    const googleToken = credentialResponse.credential;
+    console.log(googleToken);
+  };
   return (
     <div className="auth-container">
       <div className={`auth-card ${isSignup ? "signup" : "login"}`}>
@@ -12,9 +17,7 @@ export default function Login() {
           {isSignup ? "Sign up to get started" : "Login to your account"}
         </p>
 
-        {isSignup && (
-          <input type="text" placeholder="Full Name" />
-        )}
+        {isSignup && <input type="text" placeholder="Full Name" />}
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
 
@@ -25,12 +28,13 @@ export default function Login() {
         <div className="divider">OR</div>
 
         <div className="social-login">
-          <button className="google-btn">
-            <FaGoogle /> Continue with Google
-          </button>
           <button className="facebook-btn">
             <FaFacebookF /> Continue with Facebook
           </button>
+          <GoogleLogin
+            onSuccess={handleSuccess}
+            onError={() => console.log("Login Failed")}
+          />
         </div>
 
         <p className="switch-text">
@@ -41,5 +45,5 @@ export default function Login() {
         </p>
       </div>
     </div>
-  )
+  );
 }
