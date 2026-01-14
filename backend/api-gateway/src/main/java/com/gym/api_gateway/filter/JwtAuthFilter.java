9f736819021c814459a437b2972fbd885095aafb
Asfar-07 <asfarbismi@gmail.com>
2026-01-14 21:07:49 +0530

    Backend #1 ( set gateway )

---
+++ backend/api-gateway/src/main/java/com/gym/api_gateway/filter/JwtAuthFilter.java
@@ -0,0 +1,26 @@
+package com.gym.api_gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+         String path = exchange.getRequest().getPath().toString();
+//    System.out.println("Gateway path: " + path);
+
+    return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
